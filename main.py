2d2cc689b8d3489ca8c9caa11bb64a345fddb5b2
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-11-26 18:28:39 +0100

    change main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions import *
-
+import time
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -11,7 +11,59 @@ if __name__ == '__main__':
     print(first_name(list_of_names))
     list_of_full_names=first_name(list_of_names)
     print(without_dubble(list_of_full_names))
-    print(convert_all_texts(files_names))
+    #print(convert_all_texts(files_names)) #(à n'éxecuter qu'une seule fois)
     print(convert_all_texts_2(files_names))
-    print(calculf_idf("./cleaned"))
-    print(calcul_idf_final())
+
+    directory = "./cleaned"
+
+    # corriger pour avoir tous les docs
+    corpus = get_texts_from_documents(directory)  # tous les textes
+    textsNames = list_of_files(directory, 'txt')  # tous les noms
+
+    dicTF = {}
+    i = 0
+    for texte in corpus:
+        dicTF[textsNames[i]] = calcul_tf(texte)  # TF
+        i += 1
+
+    listOfWordsDeduplicated, wordsPerDoc = nbDocsPerWord(corpus)
+    dicIDF = inverse_proportion(wordsPerDoc, len(corpus))
+
+    scores = makeScore(dicTF, dicIDF)
+    matrixTfIdf = makeMatrixTfIdf(listOfWordsDeduplicated, scores, textsNames)
+
+    leastTfIdfWords = orderVectorAsc(matrixTfIdf, listOfWordsDeduplicated)
+    mostTfIdfWords = orderVectorDesc(matrixTfIdf, listOfWordsDeduplicated)
+    chirac = chiracAnalysis(corpus)
+    nation = nationAnalysis(corpus)
+    ecologie = ecologieAnalysis(corpus)
+    commonWords = allCommonWords(listOfWordsDeduplicated, corpus, scores)
+
+    while True:
+        print('Menu :')
+        print('1. Les mots les moins importants dans le corpus')
+        print('2. Les mots les plus importants')
+        print('3. Les mots les plus répétés par M.Chirac ')
+        print('4. Le président qui repète le plus le mot "Nation" ')
+        print('5. Le premier président à parler du climat ou de l écologie ')
+        print('6. Les mots communs à tous les discours')
+
+        choice = input('Entrez le numéro de votre choix : ')
+        if choice == '1':
+            print('Les mots les moins importants sont :', leastTfIdfWords)
+            time.sleep(2)
+        elif choice == '2':
+            print('Les mots les plus importants sont :', mostTfIdfWords)
+            time.sleep(2)
+        elif choice == '3':
+            print('Le(s) mot(s) le(s) plus répété(s) par M.Chirac sont :', chirac)
+            time.sleep(2)
+        elif choice == '4':
+            print('Le président qui repète le plus le mot "Nation" est :', nation)
+            time.sleep(2)
+        elif choice == '5':
+            print('Le premier président à parler du climat ou de l écologie est :', ecologie)
+            time.sleep(2)
+        elif choice == '6':
+            print('Les mots communs à tous les discours sont :', commonWords)
+            time.sleep(2)
