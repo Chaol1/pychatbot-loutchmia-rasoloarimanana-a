61adc13bdb69f9e53e9491d7a55358ff7dae68bb
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-16 08:50:23 +0100

    Modification functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
+# Score le plus élevé
 def score_eleve(mat):
-    maxi=[]
+    maxi=""
     max_tfidf=0
     for nom, vecteur in mat.items():
         for i in range (len(vecteur)):
             if vecteur[i]>max_tfidf:
                 max_tfidf=vecteur[i]
                 mot=nom
-                maxi.append(mot)
+                maxi+= mot + " "
     return maxi
