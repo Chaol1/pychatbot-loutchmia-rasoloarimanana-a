4d90f90b9a0f3558e7374088175a3a7b3bc6cc59
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-16 08:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,17 @@ if __name__ == '__main__':
     #convert_all_texts_2(files_names)
     '''
 
+    dicTF=tf
+    dicIDF=idf
+    score_tf_idf=tf_idf(dicTF,dicIDF)
+    print(score_tf_idf)
+
+    unique=mots_uniques(corpus)
+    scores=score_tf_idf
+    files_names=list_of_files(corpus, "txt")
+    
+    TF_IDF=matrice_TfIdf(corpus,mots_uniques,scores,files_names)
+    print(TF_IDF)
+    
     moins_imp=less_important(TF_IDF,unique)
     print(moins_imp)
