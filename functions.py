24808c5256ef5cd4a760a02c0656401bb572b96a
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-16 08:44:32 +0100

    Modifications de functions.py

--- functions.py
+++ functions.py
@@ -1,20 +1,27 @@
+# Mot le plus dit par Chirac
 def mots_Chirac(matrice, mots_uniques):
-    non_liste=less_important(matrice, mots_uniques)
-    mots = []
-    plus_rep=0
-    with open("cleaned/Nomination_Chirac1.txt", "r", encoding="utf-8") as f1, open("cleaned/Nomination_Chirac2.txt","r", encoding="utf-8") as f2:
+    non_liste = less_important(matrice, mots_uniques)
+    mots = ""
+    plus_rep_1 = 0
+    plus_rep_2 = 0
+    with open("cleaned/Nomination_Chirac1.txt", "r", encoding="utf-8") as f1, open("cleaned/Nomination_Chirac2.txt", "r", encoding="utf-8") as f2:
         texte1 = f1.read()
         texte2 = f2.read()
     tf1 = tf_chaine(texte1)
     tf2 = tf_chaine(texte2)
-    for mot,tf in tf1.items() :
-        if tf1[mot]>plus_rep and tf1[mot] not in non_liste:
-            plus_rep=tf1[mot]
-            mots.append(mot)
+    # Parcourir le premier fichier
+    for mot, tf in tf1.items():
+        # Si le tf du mot est supérieur au tf de base et qu'iln'est pas dans la liste
+        if tf > plus_rep_1 and mot not in non_liste:
+            plus_rep_1 = tf
+            mots = mot
+        elif tf == plus_rep_1 and mot not in non_liste:
+            mots+= mot
+    # Parcourir le deuxième fichier
     for mot, tf in tf2.items():
-        if tf2[mot] > plus_rep and tf2[mot] not in non_liste:
-            plus_rep = tf2[mot]
-            mots.append(mot)
+        if tf > plus_rep_2 and mot not in non_liste :
+            plus_rep_2 = tf
+            mots = mot
+        elif tf == plus_rep_2 and mot not in non_liste :
+            mots+=mot
     return mots
-
-  
