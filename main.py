e460dcdf559b092c6e8e40538e54681d64a8e3a1
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 22:13:40 +0100

    Changement temporaire

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from functions import *
+from functions import * #De la branche main pour avoir dic_TF, dic_IDF et dic_scores
 
 if __name__ == '__main__':
 
     corpus = "./cleaned"
+    speeches = "./speeches"
     question = "Comment une nation peut-elle prendre soin du climat ?"
-
     token_question = token(question)
     mots_communs=common_words(corpus, question)
-    print("Mots communs : ", mots_communs)
+    vect_quest=quest_tf_idf(question, corpus)
+    dic_TF = calcul_tf(corpus)
+    dic_IDF = calcul_idf_final()
+    dic_scores = tf_idf(dic_TF, dic_IDF)
+
+    similarite=calcul_similarity(dic_scores, vect_quest)
+    print("Similarité entre vecteur de la question et chaque N document :", similarite)
