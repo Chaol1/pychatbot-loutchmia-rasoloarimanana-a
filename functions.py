70b4c6e9fc7ce254b522468960acb1ec7ac08cdf
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 22:00:23 +0100

    Ajout de answer_chatbot

--- functions.py
+++ functions.py
@@ -1,23 +1,22 @@
 """ My first chatbot - LOUTCHMIA Laïli & RASOLOARIMANANA Malalasoa
-Fonction qui retourne le fichier qui a la similarité la plus proche du vecteur de la question"""
+Fonction qui améliore les réponses apportées par le chatbot"""
 
+#Fonction qui améliore les réponses apportées par le chatbot
+#Paramètres(question: question sous forme de chaîne de caractères, phrase: réponse précédente sous forme de chaîne de caractères)
+#Résultat(phrase: Réponse améliorée)
+def answer_chatbot(question,sentence):
+    #Dictionnaire avec le début des questions
+    question_starters = {
+        "Comment": "Après analyse, ",
+        "Pourquoi": "Car, ",
+        "Peux-tu": "Oui, bien sûr!",
+        "Quel": "Le voici, ",
+        "Quels": "Ce sont, ",
+        "Quand": "La date est, "
+    }
+    answer = sentence
+    for word_question, answer_beginning in question_starters.items(): #Parcours du dictionnaire question_starters
+        if question.split()[0] == word_question: #Si le premier terme de la question correspond à l'une des amorces du dictionnaire
+            answer = answer_beginning + sentence + "." #Ajout de la valeur de l'amorce choisie à la phrase ainsi qu'un point
+    return answer
 
-#Fonction qui retourne le fichier qui a la similarité la plus proche du vecteur de la question
-#Paramètres(question: question sous forme de chaîne de caractères, corpus: chemin du répertoire "cleaned")
-#Résultat(doc_pert: Nom du fichier qui a la similarité la plus proche du vecteur de la question)
-
-def doc_plus_pert(question, corpus):
-    #Utilisation du vecteur de la question
-    vect_quest=quest_tf_idf(question, corpus)
-    #Utilisation de la matrice TF-IDF
-    dic_TF = calcul_tf(corpus)
-    dic_IDF = calcul_idf_final()
-    dic_scores = tf_idf(dic_TF, dic_IDF)
-    #Utilisation de la fonction similarité
-    similarity = calcul_similarity(dic_scores, vect_quest)
-    maxi_sim = 0
-    for file, value in similarity.items(): #Parcours du dictionnaire
-        if value > maxi_sim: #value a la similarité la plus élevée
-            doc_pert = file #récupération du fichier à la similarité la plus élevée
-            maxi_sim = value
-    return doc_pert
