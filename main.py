535e90b8ec950c2c6be5c90afb4c376ac8bbe1a2
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:36:40 +0100

    Changement main

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from functions import *
-import time
+
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
-    list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
-    without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)
-    '''
-    dicTF=tf
-    dicIDF=idf
-    score_tf_idf=tf_idf(dicTF,dicIDF)
-    print(score_tf_idf)
+    corpus = "./cleaned"
 
-    unique=unique_words(corpus)
-    scores=score_tf_idf
-    files_names=list_of_files(corpus, "txt")
-    TF_IDF=matrice_TfIdf(corpus,unique_words,scores,files_names)
+    dicTF = calcul_tf(corpus)
+    print(dicTF)
+    
+    dicIDF = calcul_idf_final()
+    print(dicIDF)
+    
+    dic_scores = tf_idf(dicTF, dicIDF)
+    print(dic_scores)
+    
+    TF_IDF = matrix_TfIdf(corpus, dic_scores, files_names)
     print(TF_IDF)
+    
+    dic_mat = matrixWithWords(corpus, dic_scores, files_names)
+    print(dic_mat)
