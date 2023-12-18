dc43eb92814ec288ea77200e00d669b29d1961cc
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 22:40:51 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,23 +1,17 @@
 """ My first chatbot - LOUTCHMIA Laïli & RASOLOARIMANANA Malalasoa
-Fonction qui retourne le fichier qui a la similarité la plus proche du vecteur de la question"""
+Le fichier 'mot_question_max'' permet d'afficher le mot de la question avec le score tf-idf le plus élevé"""
 
+#Fonction 'maxi_tfidf' qui retourne le mot
+#Paramètres(vect_quest : Vecteur de la question sous forme de dictionnaire de dictionnaires)
+#Résultat(word_max : mot de la question avec le max tf-df)
 
-#Fonction qui retourne le fichier qui a la similarité la plus proche du vecteur de la question
-#Paramètres(question: question sous forme de chaîne de caractères, corpus: chemin du répertoire "cleaned")
-#Résultat(doc_pert: Nom du fichier qui a la similarité la plus proche du vecteur de la question)
-
-def doc_plus_pert(question, corpus):
-    #Utilisation du vecteur de la question
-    vect_quest=quest_tf_idf(question, corpus)
-    #Utilisation de la matrice TF-IDF
-    dic_TF = calcul_tf(corpus)
-    dic_IDF = calcul_idf_final()
-    dic_scores = tf_idf(dic_TF, dic_IDF)
-    #Utilisation de la fonction similarité
-    similarity = calcul_similarity(dic_scores, vect_quest)
-    maxi_sim = 0
-    for file, value in similarity.items(): #Parcours du dictionnaire
-        if value > maxi_sim: #value a la similarité la plus élevée
-            doc_pert = file #récupération du fichier à la similarité la plus élevée
-            maxi_sim = value
-    return doc_pert
+def maxi_tfidf(vect_quest):
+    maxi = 0.0
+    #Parcourir le vecteur de la question
+    for file, vector in vect_quest.items():
+        for word,score in vector.items():
+            #Chercher le score max et le mot qui lui correspond
+            if vector[word] > maxi:
+                maxi=vector[word]
+                word_max=word
+    return word_max
