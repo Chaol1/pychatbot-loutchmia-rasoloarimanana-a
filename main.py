408c0cbfe8a603ce3b1492b4cd13306cf0870ea4
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 15:11:41 +0100

    Changement du main

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
     #convert_all_texts_2(files_names)
     '''
     corpus = "./cleaned"
-
-    tf=calcul_tf(corpus)
-    print(tf)
+    tf = calcul_tf(corpus)
+    idf = calcul_idf_final()
+    dicTF = tf
+    dicIDF = idf
+    scores=score_tf_idf
+    files_names = list_of_files(corpus, "txt")
+    print(ecologie_climat(corpus, mots_uniques, scores, files_names))
