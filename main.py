b256e6bbffee8556d9f06b2652481d0b80bfb4d9
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 21:25:27 +0100

    Modification du main.py

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 from functions import *
-import time
+from mots_chirac import *
+
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
-    list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
-    without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)
-    '''
+    list_of_names = surname(files_names)
+    list_of_full_names = first_name(list_of_names)
+    without = without_dubble(list_of_full_names)
+
+    #Méthode TF-IDF
     corpus = "./cleaned"
-
-    tf=calcul_tf(corpus)
-    print(tf)
-
-    dicTF=tf
-    dicIDF=idf
-    score_tf_idf=tf_idf(dicTF,dicIDF)
-    print(score_tf_idf)
-
-    unique=mots_uniques(corpus)
-    scores=score_tf_idf
-    files_names=list_of_files(corpus, "txt")
-    TF_IDF=matrice_TfIdf(corpus,mots_uniques,scores,files_names)
-    print(TF_IDF)
+    dic_TF=calcul_tf(corpus)
+    nbdocparmot = nbDocperWord(corpus)
+    dic_IDF=calcul_idf_final()
+    dic_scores=tf_idf(dic_TF,dic_IDF)
+    unique = unique_words(corpus)
+    TF_IDF=matrix_TfIdf(corpus,dic_scores,files_names)
+    dic_TF_IDF=matrixWithWords(corpus, dic_scores, files_names)
     
-    chirac=mots_Chirac(TF_IDF, unique)
-    print(chirac)
+    #Fonctionnalités à développer
+
+    moins_imp=less_important(TF_IDF,unique)
+    chirac=words_Chirac(TF_IDF, unique)
+    print("Chaîne des mots de Chirac :",chirac)
