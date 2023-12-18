8d00cca619fb0142785c8d44a22e3ddd4bc5f034
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:43:15 +0100

    Changement de main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     dic_TF = calcul_tf(corpus)
     dic_IDF = calcul_idf_final()
     dic_scores = tf_idf(dic_TF, dic_IDF)
-    TF_IDF = matrice_TfIdf(corpus, dic_scores, files_names)
+    TF_IDF = matrix_TfIdf(corpus, dic_scores, files_names)
     unique = unique_words(corpus)
     score_max=score_eleve(TF_IDF,unique)
+    print(score_max)
