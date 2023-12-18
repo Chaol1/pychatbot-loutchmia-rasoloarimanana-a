ccbdb30afbaaf08e94bd1bbec8b7ad90153fd529
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:10:41 +0100

    Changement du main

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from functions import *
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
-    list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
-    without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)'''
-    
-    score_max=score_eleve(dic_TF_IDF)
-    print(score_max)
+    list_of_names = surname(files_names)
+    corpus = "./cleaned"
+    dic_TF = calcul_tf(corpus)
+    dic_IDF = calcul_idf_final()
+    dic_scores = tf_idf(dic_TF, dic_IDF)
+    dic_mat = matriceAvecMots(corpus, dic_scores, files_names)
+    nation=nation_analyse(dic_mat,list_of_names)
+   
