e8a300df01fec2ee8d1b6b78f094043be58cb560
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:20:59 +0100

    Score_eleve commenté

--- functions.py
+++ functions.py
@@ -1,11 +1,23 @@
-# Score le plus élevé
-def score_eleve(mat):
-    maxi=""
-    max_tfidf=0
-    for nom, vecteur in mat.items():
-        for i in range (len(vecteur)):
-            if vecteur[i]>max_tfidf:
-                max_tfidf=vecteur[i]
-                mot=nom
-                maxi+= mot + " "
-    return maxi
+"""My first chatbot - Laïli LOUTCHMIA & Malalasoa RASOLOARIMANANA
+Fonction retournant les mots ayant un score TF-IDF le plus élevé."""
+
+# Score TF-IDF le plus élevé
+#Paramètres(TF_IDF:Matrice TF-IDF, unique: Liste des mots sans doublons.)
+#(words:chaîne de caractères comportant tous les mots au score TF-IDF le plus élevé)
+def score_eleve(TF_IDF,unique):
+    words = " "
+    #j parcourt les mots uniques
+    j = 0
+    maxi = 0.0
+    #Parcourir les vecteurs de la matrice
+    for vector in TF_IDF:
+        sum_vector = 0
+        #Pour chaque valeur du vecteur
+        for val in vector:
+            #Ajouter la valeur à la somme
+            sum_vector += val
+        if sum_vector > maxi:
+            maxi=sum_vector
+            words+= unique[j] + " "
+        j += 1
+    return words
