2ae5208b88eb6b9ae8721e7039fd000fe6372a3b
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 15:08:19 +0100

    Ajout de commentaires

--- functions.py
+++ functions.py
@@ -1,9 +1,8 @@
 def ecologie_climat(corpus, mots_uniques, scores, files_names):
-    mat=matriceAvecMots(corpus, mots_uniques, scores, files_names)
-    cpt=0
-    print(mat["climat"])
-    for i in range (len(mat["climat"])):
+    mat=matriceAvecMots(corpus, mots_uniques, scores, files_names) #Matrice TF-IDF formatée avec le mot et son score TF-IDF
+    cpt=0 
+    for i in range (len(mat["climat"])): #Liste contenant les scores TF-IDF de chaque document sur le mot "climat" 
         if mat["climat"][i]!=0.0:
             cpt=i
-    list1=surname(files_names)
-    return (list1[cpt])
+    list1=surname(files_names) #Liste des noms de présidents
+    return (list1[cpt]) #Retourne le nom du président qui parle de "climat"
