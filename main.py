5994bfdc07d759e83c7b174291fd3fcccd2ad838
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 22:43:50 +0100

    Mise à jour main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from functions import *
-import time
+from functions import * 
+#Importer aussi les fonctions des branches tf, IDF et tf_idf_quest pour avoir dic_TF, dic_IDF et dic_scores
+
 if __name__ == '__main__':
-    corpus="./cleaned"
-    print(doc_plus_pert("Comment vont les messieurs ?", corpus))
+
+    corpus = "./cleaned"
+    speeches = "./speeches"
+    question = "Comment une nation peut-elle prendre soin du climat ?"
+
+    token_question = token(question)
+    common=common_words(corpus, question)
+    vect_quest=quest_tf_idf(question, corpus)
+    dic_TF = calcul_tf(corpus)
+    dic_IDF = calcul_idf_final()
+    dic_scores = tf_idf(dic_TF, dic_IDF)
+
+    similar=calcul_similarity(dic_scores, vect_quest)
+    document = doc_plus_pert(question, corpus)
+    
+    #Fonction qui nous intéresse
+    maxi_word = maxi_tfidf(vect_quest)
+    print("Mot ayant le TF-IDF le plus élevé :", maxi_word)
