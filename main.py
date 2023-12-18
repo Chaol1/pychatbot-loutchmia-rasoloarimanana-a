0e0e725a33f19a882fb87b422b7de2bd5c111225
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 21:20:24 +0100

    Changement de main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from functions import *
-import time
+
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
-    list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
-    without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)
-    '''
+    list_of_names = surname(files_names)
+    
     corpus = "./cleaned"
-    tf = calcul_tf(corpus)
-    idf = calcul_idf_final()
-    dicTF = tf
-    dicIDF = idf
-    scores=score_tf_idf
-    files_names = list_of_files(corpus, "txt")
-    print(ecologie_climat(corpus, mots_uniques, scores, files_names))
+    dic_TF = calcul_tf(corpus)
+    dic_IDF = calcul_idf_final()
+    dic_scores = tf_idf(dic_TF, dic_IDF)
+    dic_mat = matrixWithWords(corpus, dic_scores, files_names)
+    TF_IDF=
+    nation=nation_analyse(dic_mat,list_of_names)
+    chirac=words_Chirac(TF_IDF, unique)
+    climat=climate_names(dic_TF_IDF,list_of_names)
+    print("Les présidents écologie/climat :", climat)
