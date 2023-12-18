81840f5a6c2b729969f0c9bf6cb0150276ab4d20
malalas0a <150929855+malalas0a@users.noreply.github.com>
2023-12-18 21:17:48 +0100

    Changement du main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from functions import *
-import time
+from tf_method import *
+
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
     list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
     without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)
-    '''
+    
     corpus = "./cleaned"
-
-    tf=calcul_tf(corpus)
-    print(tf)
+    dic_TF=calcul_tf(corpus)
+    print("Dic TF :",dic_TF)
