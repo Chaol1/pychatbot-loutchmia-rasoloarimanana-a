88dab0b5c533f2a0856173e9853524c691267ae2
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 17:05:42 +0100

    Changement temporaire

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
+"""My first chatbot - LOUTCHMIA Laïli & RASOLOARIMANANA Malalasoa
+Création du dictionnaire des scores TF-IDF et de la matrice TF-IDF"""
+
 #Calcul du score TF-IDF
+#Paramètres(dicTF: dictionnaire contenant les scores TF de chaque mot, dicIDF: dictionnaire contenant les scores IDF de chaque mot)
+#Résultat(scores: dictionnaires contenant les scores TF-IDF de chaque mot)
 
 def tf_idf(dicTF,dicIDF):
     scores = {}
@@ -13,48 +18,48 @@ def tf_idf(dicTF,dicIDF):
 
 # Liste des mots dédupliqués
 
-def mots_uniques(corpus):
+def unique_words(corpus):
     list_of_words = []
     files_names = list_of_files(corpus, "txt")
     #Parcourir chaque fichier de la liste
-    for fichier in files_names:
-        chemin = os.path.join("./cleaned", fichier)
-        with open(chemin, "r", encoding="utf-8") as f1:
-            contenu = f1.read()
-            mots = contenu.split()
+    for file in files_names:
+        path1 = os.path.join("./cleaned", file)
+        with open(path1, "r", encoding="utf-8") as f1:
+            content = f1.read()
+            words = content.split()
         #Pour chaque mot
-        for mot in mots:
+        for word in words:
             #Ajouter tous les mots de tous les fichiers
-            list_of_words.append(mot)
+            list_of_words.append(word)
     #Initialiser une liste vide
     unique = []
     #Pour chaque mot de la liste de tous les mots
-    for mot in list_of_words:
+    for word in list_of_words:
         #Si le mot n'est pas dans la liste de mots unique
-        if mot not in unique:
+        if word not in unique:
             #Ajouter le mot
-            unique.append(mot)
+            unique.append(word)
     return unique
 
 #Création de la matrice TF-IDF
 
-def matrice_TfIdf(corpus,mots_uniques,scores,files_names):
+def matrix_TfIdf(corpus,mots_uniques,scores,files_names):
     M = []
     #Utiliser la fonction précédente pour avoir les mots uniques
-    liste_mots=mots_uniques(corpus)
+    list_of_words=unique_words(corpus)
     #Pour chaque mot de la liste
-    for mot in liste_mots:
+    for word in list_of_words:
         #Initialiser une liste vecteur numérique
-        vecteur_num= []
+        vector_num= []
         #Parcourir chaque fichier
-        for fichier in files_names:
+        for file in files_names:
             #Si le mot est dans le dic des scores TF-IDF
-            if mot in scores[fichier]:
+            if word in scores[file]:
                 #Ajouter à son vecteur de scores le vecteur numérique
-                vecteur_num.append(scores[fichier][mot])
+                vector_num.append(scores[file][word])
             else:
                 #Sinon ajouter un score de 0
-                vecteur_num.append(0.0)
+                vector_num.append(0.0)
         #Ajouter à la matrice le vecteur de scores pour chaque mot
-        M.append(vecteur_num)
+        M.append(vector_num)
     return M
