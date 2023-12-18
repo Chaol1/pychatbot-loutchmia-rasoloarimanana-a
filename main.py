c5d2a74b04f6f00c585cbcc98e1860b105523a95
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:41:19 +0100

    Changement du main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     dic_TF = calcul_tf(corpus)
     dic_IDF = calcul_idf_final()
     dic_scores = tf_idf(dic_TF, dic_IDF)
-    dic_mat = matriceAvecMots(corpus, dic_scores, files_names)
+    dic_mat = matrixWithWords(corpus, dic_scores, files_names)
     nation=nation_analyse(dic_mat,list_of_names)
+    print(nation)
    
