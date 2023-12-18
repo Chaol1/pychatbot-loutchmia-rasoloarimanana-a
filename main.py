e989c63c185a6b77cd4a8409fa29e9d97c65c61e
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 20:23:37 +0100

    Changement de main

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from functions import *
 if __name__ == '__main__':
-    '''
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
-    list_of_names=surname(files_names)
-    print(list_of_names)
-    list_of_full_names=first_name(list_of_names)
-    print(list_of_full_names)
-    without=without_dubble(list_of_full_names)
-    print(without)
-    #convert_all_texts(files_names) (à ne plus exécuter)"""
-    #convert_all_texts_2(files_names)'''
-    
-    score_max=score_eleve(dic_TF_IDF)
-    print(score_max)
+    corpus = "./cleaned"
+    dic_TF = calcul_tf(corpus)
+    dic_IDF = calcul_idf_final()
+    dic_scores = tf_idf(dic_TF, dic_IDF)
+    TF_IDF = matrice_TfIdf(corpus, dic_scores, files_names)
+    unique = unique_words(corpus)
+    score_max=score_eleve(TF_IDF,unique)
