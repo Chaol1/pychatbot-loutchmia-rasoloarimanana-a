5fc67333b3ba0aa9348e426039ebc74f875829cb
Chaol1 <151723514+Chaol1@users.noreply.github.com>
2023-12-18 21:37:34 +0100

    Changement du main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from functions import *
 import time
 if __name__ == '__main__':
-    print(token("Comment vont-ils ?"))
+    print(quest_final("Comment vont les messieurs les ?", corpus))
+    print(quest_tf_idf("Comment vont les messieurs les ?", corpus))
